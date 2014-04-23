1da34708816a8ca442dbf104ebde0b1d89cb2dc5
thurst306 <thurst306@gmail.com>
2014-04-23 08:57:10 -0700

    Added the animation
    This should work I think.
    
    Some formatting

--- uprocket.js
+++ uprocket.js
@@ -42,14 +42,22 @@ console.log('Uprocket started');
 
 //document.getElementByClassName('arrow").style.overflow="visible";
 $("*.content .midcol,.content .commentarea .comment").css("overflow", "visible", "important");
+
 $.keyframe.define([{
-    name: 'upMoon',
-    '0%': { 'transform': 'translate(0,0)', 'opacity': '1' },
-    '10%': { 'transform': 'translate(4px,0) rotate(5deg)' },
-    '15%': { 'transform': 'translate(-4px,0) otate(-15deg)' },
-    '20%': { 'transform': 'translate(0,0)' },
-    '50%': { 'transform': 'translate(0,10px)', 'opacity': '1' },
-    '88%': { 'transform': 'translate(0,-140px)', 'opacity': '0' },
-    '94%': { 'transform': 'translate(0,14px)', 'opacity': '0' },
-    '100%': { 'transform': 'translate(0,0)', 'opacity': '1' }
+  name: 'upMoon',
+  '0%': { 'transform': 'translate(0,0)', 'opacity': '1' },
+  '10%': { 'transform': 'translate(4px,0) rotate(5deg)' },
+  '15%': { 'transform': 'translate(-4px,0) otate(-15deg)' },
+  '20%': { 'transform': 'translate(0,0)' },
+  '50%': { 'transform': 'translate(0,10px)', 'opacity': '1' },
+  '88%': { 'transform': 'translate(0,-140px)', 'opacity': '0' },
+  '94%': { 'transform': 'translate(0,14px)', 'opacity': '0' },
+  '100%': { 'transform': 'translate(0,0)', 'opacity': '1' }
 }]);
+
+$(".arrow.upmod").playKeyFrame({
+  name: 'upMoon',
+  duration: 1500,
+  repeat: 1,
+  timingFunction: 'ease'
+});
