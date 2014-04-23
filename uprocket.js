68ddfe70ea44f8e1cd11f7c9cb773ec600454abe
thurst306 <thurst306@gmail.com>
2014-04-23 12:08:05 -0700

    Removed comments to drop file size
    Ever so slightly improving performance I suppose. I feel bad about taking out /u/alystair's code, though. But it's there! Also it runs on both /r/dogecoin and /r/dogecoin/comments pages now. Not sure why it was behaving weird earlier.

--- uprocket.js
+++ uprocket.js
@@ -1,43 +1,13 @@
 // ==UserScript==
-// @name       Uprocket!
-// @namespace  https://github.com/thurst306/Uprocket
-// @version    1
-// @description  Make the /r/dogecoin uprocket fly!
-// @match      http://www.reddit.com/r/dogecoin/*
-// @require    http://thurstshouse.com/warehouse/Hosting/jquery.keyframes.min.js
+// @name		Uprocket!
+// @namespace	https://github.com/thurst306/Uprocket
+// @version		1
+// @description	Make the /r/dogecoin uprocket fly!
+// @match		http://www.reddit.com/r/dogecoin/*
+// @require		http://thurstshouse.com/warehouse/Hosting/jquery.keyframes.min.js
 // ==/UserScript==
 
 console.log('Uprocket started');
-
-//CSS from /u/alystair here:
-/*.content .midcol,.content .commentarea .comment {overflow:visible !important;}
-.arrow.upmod{
-  animation: upMoon ease 1.5s;
-  animation-iteration-count: 1;
-  -webkit-animation: upMoon ease .7s;
-  -webkit-animation-iteration-count: 1;
-}
-@keyframes upMoon{
-  0% { transform:translate(0,0); opacity:1; }
-  10% { transform:translate(4px,0) rotate(5deg); }
-  15% { transform:translate(-4px,0) rotate(-15deg); }
-  20% { transform:translate(0,0); }
-  50% { transform:translate(0,10px); opacity:1; }
-  88% { transform:translate(0,-140px); opacity:0; }
-  94% { transform:translate(0,14px); opacity:0;}
-  100% { transform:translate(0,0); opacity:1; }
-}
-
-@-webkit-keyframes upMoon{
-  0% { -webkit-transform:translate(0,0); opacity:1; }
-  10% { -webkit-transform:translate(4px,0) rotate(5deg); }
-  15% { -webkit-transform:translate(-4px,0) rotate(-15deg); }
-  20% { -webkit-transform:translate(0,0); }
-  50% { -webkit-transform:translate(0,10px); opacity:1; }
-  88% { -webkit-transform:translate(0,-140px); opacity:0; }
-  94% { -webkit-transform:translate(0,14px); opacity:0;}
-  100% { -webkit-transform:translate(0,0); opacity:1; }
-}*/
 
 $("*.content .midcol,.content .commentarea .comment").css("overflow", "visible", "important");
 
@@ -60,4 +30,4 @@ $(".arrow.up").click(function(){
         repeat: 1,
         timingFunction: 'ease'
     });
-})
+});
